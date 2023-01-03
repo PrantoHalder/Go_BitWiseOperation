af006c7bb8e00aa7a137f1dbd060b3373f29078c
Pranto Halder <56364262+PrantoHalder@users.noreply.github.com>
2023-01-03 17:36:42 +0600

    Add files via upload

---
+++ bitWiseOperation.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func bitWiseOperaton(){
+var x int = 18
+var y int = 17
+
+fmt.Println("x & y = ",x & y)
+fmt.Println("x | y = ",x | y)
+fmt.Println("x ^ y = ",x^y)
+
+}
